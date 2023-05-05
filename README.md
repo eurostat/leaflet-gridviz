8fdfbe3d6aadc34b6b6a903dfe49282ec5b8d621
joewdavies <jwd101@gmail.com>
2023-05-05 16:42:00 +0200

    remove artifacts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leaflet-gridviz
+
 A plugin for Leaflet to show gridviz maps
+
+With node.js 14.20.1:
+
+`npm install`
+`npm start`
--- src/main.js
+++ src/main.js
@@ -11,45 +11,50 @@ import { interpolateInferno } from 'd3-scale-chromatic'
  * 
  */
 L.GridvizLayer = function () {
-    this.onLayerDidMount = function (info) {
-        // after canvas is attached/added to the map
+    /**
+     * @description Fires after leaflet layer canvas is attached/added to the map
+     *
+     */
+    this.onLayerDidMount = function () {
         this.buildGridVizApp()
     }
+
+    /**
+     * @description Fires before layer is removed from the map
+     *
+     */
     this.onLayerWillUnmount = function () {
-        // before layer is removed from the map
+        // cleanup?
     }
+
+    /**
+     * @description Fires before layer is removed from the map
+     *
+     */
     this.setData = function (data) {
         // -- custom data set
         this.needRedraw() // -- call to drawLayer
     }
+
+    /**
+     * @description Fires when layer is drawn , info contains view parameters like bounds, size, canvas etc.
+     *
+     */
     this.onDrawLayer = function (info) {
-        // -- when layer is drawn , info contains view parameters like bounds, size, canvas etc.
+        //
         console.log(info)
     }
 
+    /**
+     * @description build a gridviz app and add a layer to it
+     */
     this.buildGridVizApp = function () {
         this.app = new gridviz.App(this._canvas, { w: window.innerWidth, h: window.innerHeight })
             .setGeoCenter({ x: 4000000, y: 2960000 })
             .setZoomFactor(1000)
             .setZoomFactorExtent([30, 7000])
             .setBackgroundColor('black')
-            .setLabelLayer(
-                gridviz.getEuronymeLabelLayer('EUR', 50, {
-                    ex: 2,
-                    fontFamily: 'mfLeg',
-                    exSize: 0.9,
-                    color: () => 'black',
-                    haloColor: () => '#ffffff',
-                    haloWidth: () => 3,
-                })
-            )
-            .setBoundaryLayer(
-                gridviz.getEurostatBoundariesLayer({
-                    scale: '10M',
-                    col: '#fff5',
-                    lineDash: () => [],
-                })
-            )
+
             .addMultiScaleTiledGridLayer(
                 [1000, 2000, 5000, 10000, 20000, 50000, 100000],
                 (r) =>
